f2401992422e67be0cd9baf132403f41d07eaea3
hayzedzee <azeezahayomide@gmail.com>
2022-04-11 04:11:30 -0700

    my name

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab1work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    //Console.WriteLine("Hello World!");
+
+            string myName = " My name is Azeezah Musa";
+            string myTrack = " Backend Track,";
+            string myReason = "my goal is to become a backend developer in six months.";
+
+
+            Console.Write("Hello");
+            Console.Write(myName);
+            Console.Write(" I am in");
+            Console.Write(myTrack);
+            Console.WriteLine(myReason);
+        }
+    }
+}
